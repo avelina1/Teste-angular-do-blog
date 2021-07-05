c4a2fbd1640d45479ff142b2bed2ee0e9cb6680c
unknown <avelinafilipe@hotmail.com>
2021-07-05 14:38:38 -0300

    implementação do put e delet no tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -16,11 +16,26 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
+  refreshToken(){
+    this.token={headers: new HttpHeaders().set('Authorization', environment.token)}
+  }
   getAllTema(): Observable<Tema[]>{
     return this.http.get<Tema[]>('https://blogavelina.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogavelina.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogavelina.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogavelina.herokuapp.com/tema', tema,this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://blogavelina.herokuapp.com/tema/${id}`, this.token)
   }
 }
